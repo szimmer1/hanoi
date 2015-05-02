ae594548c485d611477593aaee53008daeb6e70a
Shahar Zimmerman <szimmer1@ucsc.edu>
2015-05-02 14:25:45 -0700

    finished recursive func

--- hanoi.py
+++ hanoi.py
@@ -1,28 +1,40 @@
 class HanoiException(Exception):
-    def __init__(self, message = ""):
+    def __init__(self, message = "moved big on small"):
         self.message = message
     def __str__(self):
         return repr(self.message)
 
 
 class Hanoi():
-
-   def __init__(self, pegs = 3, disks = 3):
+   def __init__(self, disks = 3):
+       self.moves = 0
        self.disks = disks
-       self.pegs = [[] for _ in range(0,pegs)]
+       self.pegs = [[],[],[]]
        for i in range(1,disks+1):
            self.pegs[0].append(i)
 
+   def size(self):
+       return self.disks
+
    def move(self, from_peg, to_peg):
-       f = self.pegs[from_peg]
+       self.moves += 1
        t = self.pegs[to_peg]
+       f = self.pegs[from_peg]
        disk = f.pop()
        if len(t) > 0 and disk < t[len(t)-1]:
-           raise HanoiException("Moved large on small")
+           raise HanoiException(
+                   dict(
+                        from_peg=dict(peg=from_peg,state=f),
+                        to_peg=dict(peg=to_peg,state=t),
+                        bottom_disk=t[len(t)-1],
+                        top_disk=disk
+                        )
+                   )
        else:
            t.append(disk)
 
    def out(self):
+       print "Move "+str(self.moves)
        format_space_str = "{0:^"+str(self.disks*2+1)+"s}"
        for i in range( len(max(self.pegs,key = lambda peg:len(peg))) - 1, -1, -1):
            for peg in self.pegs:
@@ -34,4 +46,5 @@ class Hanoi():
            print
        for peg in range(0,len(self.pegs)):
            print format_space_str.format(str(peg)),
+       print
        print
--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 import sys
 import hanoi
 
-if len(sys.argv) < 3:
-    print "Usage: python hanoi [pegs] [disks]"
+def solve(obj,n,f,t):
+    if n is 1:
+        obj.move(f,t)
+        obj.out()
+        return
+    u = 3 - f - t
+    solve(obj,n-1,f,u)
+    obj.move(f,t)
+    obj.out()
+    solve(obj,n-1,u,t)
+
+if len(sys.argv) < 2:
+    print "Usage: python hanoi [disks]"
     sys.exit()
 
 args = [int(i) for i in sys.argv[1:]]
-towers = hanoi.Hanoi(args[0], args[1])
-towers.out()
+towers = hanoi.Hanoi(args[0])
+solve(towers,towers.size(),0,2)
